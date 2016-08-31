5e30aa761df21c2f41713f37519e43db5ea50766
ky <ky@DESKTOP-BHI4ELL>
2016-08-31 15:22:24 +0900

    Mcontroller 작성중

--- minimall/src/com/minimall/controller/Mcontroller.java
+++ minimall/src/com/minimall/controller/Mcontroller.java
@@ -8,23 +8,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/Mcontroller")
-public class Mcontroller extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+public class Mcontroller extends HttpServlet {	
 
     public Mcontroller() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("doGet Mcontroller.java");
+		doPro(request, response);
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		System.out.println("doPost Mcontroller.java");
+		doPro(request, response);
 	}
-
+	protected void doPro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPro Mcontroller.java");
+		
+		String fullUrl = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String cutUrl = fullUrl.substring( request.getContextPath().length());
+		
+		System.out.println(cutUrl);
+		
+		if(cutUrl.equals("/mIn/mInsert.mo")){
+			
+			System.out.println("조건1분기");
+			
+		}
+	}
 }
