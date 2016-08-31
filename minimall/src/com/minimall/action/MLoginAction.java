eb336cf550123515239da9f368556a312ded5504
ky <ky@DESKTOP-BHI4ELL>
2016-08-31 17:27:39 +0900

    네임 레벨 세션영역 처리

--- minimall/src/com/minimall/action/MLoginAction.java
+++ minimall/src/com/minimall/action/MLoginAction.java
@@ -2,6 +2,7 @@ package com.minimall.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.minimall.dao.MemberDao;
 import com.minimall.dto.MemberDto;
@@ -12,7 +13,7 @@ public class MLoginAction implements ActionInterFace {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+		HttpSession session=request.getSession();
 		String mId = request.getParameter("mId");
 		String mPw = request.getParameter("mPw");
 		 
@@ -23,10 +24,15 @@ public class MLoginAction implements ActionInterFace {
 			
 			System.out.println("로그인 성공");
 			
-			System.out.println(chkMember.hashCode());
-			
 			System.out.println(chkMember.getm_name());
 			System.out.println(chkMember.getm_level());
+			
+			String loginLevel = chkMember.getm_level();
+			String loginName = chkMember.getm_name();
+			
+			
+			session.setAttribute("loginLevel", loginLevel);
+			session.setAttribute("loginName", loginName);
 			
 		}else{
 			
