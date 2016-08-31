b70df5d15781f443fef760a4f64ed97b8768a6f3
ky <ky@DESKTOP-BHI4ELL>
2016-08-31 16:03:02 +0900

    MInsertAction 초기 파일 생성

---
+++ minimall/src/com/minimall/action/MemberInsertAction.java
@@ -0,0 +1,26 @@
+package com.minimall.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.minimall.dao.MemberDao;
+import com.minimall.dto.MemberDto;
+import com.minimall.forward.ActionForward;
+import com.minimall.inter.ActionInterFace;
+
+public class MemberInsertAction implements ActionInterFace {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		String mId = request.getParameter(arg0);
+		MemberDto m = new MemberDto();
+		MemberDao mDao = new MemberDao();
+		m.setm_id(mId);
+		
+		mDao.insertMember(m);	
+		
+		return null;
+	}
+
+}
