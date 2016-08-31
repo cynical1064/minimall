dd079b5ea5e98168e33b0b70fbd8f48932b8cdfa
ky <ky@DESKTOP-BHI4ELL>
2016-08-31 17:22:07 +0900

    dto수정 및 로그인 처리 중간

--- minimall/src/com/minimall/action/MLoginAction.java
+++ minimall/src/com/minimall/action/MLoginAction.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.minimall.dao.MemberDao;
+import com.minimall.dto.MemberDto;
 import com.minimall.forward.ActionForward;
 import com.minimall.inter.ActionInterFace;
 
@@ -16,12 +17,19 @@ public class MLoginAction implements ActionInterFace {
 		String mPw = request.getParameter("mPw");
 		
 		MemberDao dao = new MemberDao();
-		int chkMember = dao.userCheck(mId, mPw);
+		MemberDto chkMember = dao.userCheck(mId, mPw);
 		
-		if(chkMember==1){
+		if(chkMember != null){
+			
 			System.out.println("로그인 성공");
 			
+			System.out.println(chkMember.hashCode());
+			
+			System.out.println(chkMember.getm_name());
+			System.out.println(chkMember.getm_level());
+			
 		}else{
+			
 			System.out.println("로그인 실패");
 			
 		}
--- minimall/src/com/minimall/dto/MemberDto.java
+++ minimall/src/com/minimall/dto/MemberDto.java
@@ -2,55 +2,55 @@ package com.minimall.dto;
 
 public class MemberDto {
 
-	public String m_id = null;
-	public String m_pw = null;
-	public String m_level = null;
-	public String m_name = null;
-	public String m_email = null;
-	public String m_date = null;
-	public String m_addr = null;
+	private String m_id;
+	private String m_pw;
+	private String m_level;
+	private String m_name;
+	private String m_email;
+	private String m_date;
+	private String m_addr;
 	
 	public String getm_id() {
 		return m_id;
 	}
 	public void setm_id(String m_id) {
-		m_id = m_id;
+		this.m_id = m_id;
 	}
 	public String getm_pw() {
 		return m_pw;
 	}
 	public void setm_pw(String m_pw) {
-		m_pw = m_pw;
+		this.m_pw = m_pw;
 	}
 	public String getm_level() {
 		return m_level;
 	}
 	public void setm_level(String m_level) {
-		m_level = m_level;
+		this.m_level = m_level;
 	}
 	public String getm_name() {
 		return m_name;
 	}
 	public void setm_name(String m_name) {
-		m_name = m_name;
+		this.m_name = m_name;
 	}
 	public String getm_email() {
 		return m_email;
 	}
 	public void setm_email(String m_email) {
-		m_email = m_email;
+		this.m_email = m_email;
 	}
 	public String getm_date() {
 		return m_date;
 	}
 	public void setm_date(String m_date) {
-		m_date = m_date;
+		this.m_date = m_date;
 	}
 	public String getm_addr() {
 		return m_addr;
 	}
 	public void setm_addr(String m_addr) {
-		m_addr = m_addr;
+		this.m_addr = m_addr;
 	}
 
 
