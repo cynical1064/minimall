1fe70a1c50ce02bc1517849aef71f0a70f9bd1d0
202-07 <202-07@202-07-PC>
2016-08-31 15:52:45 +0900

    mDto수정

--- minimall/src/com/minimall/dto/MemberDto.java
+++ minimall/src/com/minimall/dto/MemberDto.java
@@ -2,56 +2,55 @@ package com.minimall.dto;
 
 public class MemberDto {
 
-	public String m_id = null;
-	public String m_pw = null;
-	public String m_level = null;
-	public String m_name = null;
-	public String m_email = null;
-	public String m_date = null;
-	public String m_addr = null;
-	
-	public String getm_id() {
+	private String m_id = null;
+	private String m_pw = null;
+	private String m_level = null;
+	private String m_name = null;
+	private String m_email = null;
+	private String m_date = null;
+	private String m_addr = null;
+	public String getM_id() {
 		return m_id;
 	}
-	public void setm_id(String m_id) {
-		m_id = m_id;
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
 	}
-	public String getm_pw() {
+	public String getM_pw() {
 		return m_pw;
 	}
-	public void setm_pw(String m_pw) {
-		m_pw = m_pw;
+	public void setM_pw(String m_pw) {
+		this.m_pw = m_pw;
 	}
-	public String getm_level() {
+	public String getM_level() {
 		return m_level;
 	}
-	public void setm_level(String m_level) {
-		m_level = m_level;
+	public void setM_level(String m_level) {
+		this.m_level = m_level;
 	}
-	public String getm_name() {
+	public String getM_name() {
 		return m_name;
 	}
-	public void setm_name(String m_name) {
-		m_name = m_name;
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
 	}
-	public String getm_email() {
+	public String getM_email() {
 		return m_email;
 	}
-	public void setm_email(String m_email) {
-		m_email = m_email;
+	public void setM_email(String m_email) {
+		this.m_email = m_email;
 	}
-	public String getm_date() {
+	public String getM_date() {
 		return m_date;
 	}
-	public void setm_date(String m_date) {
-		m_date = m_date;
+	public void setM_date(String m_date) {
+		this.m_date = m_date;
 	}
-	public String getm_addr() {
+	public String getM_addr() {
 		return m_addr;
 	}
-	public void setm_addr(String m_addr) {
-		m_addr = m_addr;
+	public void setM_addr(String m_addr) {
+		this.m_addr = m_addr;
 	}
-
-
+	
+	
 }
