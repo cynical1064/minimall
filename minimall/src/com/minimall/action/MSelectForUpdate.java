b93ef81eba9d031e751b5d5e9ac6a44286d6f331
JK892015 <glty89@naver.com>
2016-09-01 15:50:53 +0900

    action 추가 - 수정 화면 - 수정 처리

---
+++ minimall/src/com/minimall/action/MSelectForUpdate.java
@@ -0,0 +1,30 @@
+package com.minimall.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.minimall.dao.MemberDao;
+import com.minimall.dto.MemberDto;
+import com.minimall.forward.ActionForward;
+import com.minimall.inter.ActionInterFace;
+
+public class MSelectForUpdate implements ActionInterFace {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		MemberDao dao = new MemberDao();
+		String mid = request.getParameter("send_id");
+		
+		System.out.println(mid);
+		MemberDto m =  dao.mSelectforUpdate(mid);
+		request.setAttribute("m", m);
+		
+		ActionForward forward = new ActionForward();
+		
+		forward.setRedirect(false);
+		forward.setPath("/mupdate/m_update_form.jsp");
+		return forward;
+	}
+
+}
