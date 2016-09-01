8cb06ce75686b897c618d9547ef0eeb05cd568b1
202-07 <202-07@202-07-PC>
2016-09-01 15:31:40 +0900

    REB

---
+++ minimall/src/com/minimall/action/REBModifyView.java
@@ -0,0 +1,31 @@
+package com.minimall.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.board.db.BoardDAO;
+import net.board.db.BoardBean;
+
+public class REBModifyView implements Action {
+	 public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception{
+		 	ActionForward forward = new ActionForward();
+		 	request.setCharacterEncoding("euc-kr");
+	   		
+			REBoardDAO boarddao=new REBoardDAO();
+		   	REBoardDto boarddata=new REBoardDto();
+		   	
+			int num=Integer.parseInt(request.getParameter("num"));
+		   	boarddata=boarddao.getDetail(num);
+		   	
+		   	if(boarddata==null){
+		   		System.out.println("(수정)상세보기 실패");
+		   		return null;
+		   	}
+		   	System.out.println("(수정)상세보기 성공");
+		   	
+		   	request.setAttribute("boarddata", boarddata);
+		   	forward.setRedirect(false);
+	   		forward.setPath("./qna_board_modify.jsp");
+	   		return forward;
+	 }
+}
