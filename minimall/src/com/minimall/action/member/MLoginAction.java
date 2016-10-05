8073566d4030908d19f3f8ec7840484f8775945e
ky <ky@DESKTOP-BHI4ELL>
2016-10-05 10:01:48 +0900

    중복로그인 체크

--- minimall/src/com/minimall/action/member/MLoginAction.java
+++ minimall/src/com/minimall/action/member/MLoginAction.java
@@ -8,6 +8,7 @@ import com.minimall.dao.MemberDao;
 import com.minimall.dto.MemberDto;
 import com.minimall.forward.ActionForward;
 import com.minimall.inter.ActionInterFace;
+import com.minimall.listener.LoginCheck;
 
 public class MLoginAction implements ActionInterFace {
 
@@ -21,21 +22,30 @@ public class MLoginAction implements ActionInterFace {
 		 
 		MemberDao dao = new MemberDao();		
 		MemberDto chkMember = dao.userCheck(mId, mPw);
+		LoginCheck loginCheck=LoginCheck.getInstance();
 
 		//리턴된 멤버객체를 받아 
-		if(chkMember != null){			
-			System.out.println("로그인 성공");			
-			System.out.println(chkMember.getm_name());
-			System.out.println(chkMember.getm_level());			
-			String loginLevel = chkMember.getm_level();
-			String loginName = chkMember.getm_name();
-			String loginId = chkMember.getm_id();
-			//객체안에 담긴 값을 변수에 담고			 
-			session.setAttribute("loginLevel", loginLevel);
-			session.setAttribute("loginName", loginName);
-			session.setAttribute("loginId", loginId);
-			session.setAttribute("loginChk", 2);
-			//세션영역에 셋팅
+		if(chkMember != null){
+			if(!(loginCheck.isUsing(chkMember.getm_id()))) { 
+				System.out.println("로그인 성공");			
+				System.out.println(chkMember.getm_name());
+				System.out.println(chkMember.getm_level());			
+				String loginLevel = chkMember.getm_level();
+				String loginName = chkMember.getm_name();
+				String loginId = chkMember.getm_id();
+				
+				//객체안에 담긴 값을 변수에 담고			 
+				session.setAttribute("loginLevel", loginLevel);
+				session.setAttribute("loginName", loginName);
+				session.setAttribute("loginId", loginId);
+				session.setAttribute("loginChk", 2);
+				loginCheck.setSession(session, loginId);
+				String usingId = loginCheck.getUserID(session);
+				System.out.println(usingId+" <--usingId");
+				//세션영역에 셋팅
+			}else{				
+				session.setAttribute("loginChk", 3);
+			}
 		}else{
 			
 			System.out.println("로그인 실패");
